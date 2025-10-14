37276d7f9eb29a7d51b1ef710e912d46449bd344
abdelelrahmanali.mohamed <abdelrahmanali.mohamed@superhog.com>
2025-10-14 14:15:52 +0200

    update 2

---
+++ Infrastructure/CompanyPost.Infrastructure/Migrations/20251014121256_RemoveIdFromBridgeTableSysUsersCompanyAndMakeFKsOnlyPK.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyPost.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIdFromBridgeTableSysUsersCompanyAndMakeFKsOnlyPK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_sys_users_companies",
+                table: "sys_users_companies");
+
+            migrationBuilder.RenameColumn(
+                name: "username",
+                table: "sys_users",
+                newName: "user_name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_sys_users_companies",
+                table: "sys_users_companies",
+                columns: new[] { "sys_user_id", "company_id" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_sys_users_companies",
+                table: "sys_users_companies");
+
+            migrationBuilder.RenameColumn(
+                name: "user_name",
+                table: "sys_users",
+                newName: "username");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_sys_users_companies",
+                table: "sys_users_companies",
+                columns: new[] { "sys_user_id", "company_id", "id" });
+        }
+    }
+}
