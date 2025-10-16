096d267d5cd348fc77e5786784d2dc353ffbf2e1
abdelelrahmanali.mohamed <abdelrahmanali.mohamed@superhog.com>
2025-10-16 17:43:11 +0200

    update 1

--- Application/CompanyPosts.Application/CQRS/Handlers/Query/VerifyToken/VerifyTokenHandler.cs
+++ Application/CompanyPosts.Application/CQRS/Handlers/Query/VerifyToken/VerifyTokenHandler.cs
@@ -9,18 +9,18 @@ internal sealed class VerifyTokenHandler
 	{
 		_contextAccessor = contextAccessor;
 	}
-	public async Task<VerifyTokenResultDTO> Handle(VerifyTokenQuery request, CancellationToken cancellationToken)
+	public Task<VerifyTokenResultDTO> Handle(VerifyTokenQuery request, CancellationToken cancellationToken)
 	{
 		var user = _contextAccessor.HttpContext?.User;
 
 		if (user?.Identity is not { IsAuthenticated: true })
-			return new VerifyTokenResultDTO (IsValid : false, Guid.Empty , Message :"Invalid or expired token." );
-
+			return Task.FromResult(new VerifyTokenResultDTO(IsValid: false, null, Message: "Invalid or expired token."));
+		
 		var userIdClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
+		
 		if (!Guid.TryParse(userIdClaim, out var userId))
-			return new VerifyTokenResultDTO(IsValid: false, Guid.Empty, Message: "UserId not in the token.");
-
-		return new VerifyTokenResultDTO(IsValid: true, userId, Message: "Token is valid");
+			return Task.FromResult(new VerifyTokenResultDTO(IsValid: false, null, Message: "UserId not in the token."));
+		
+		return Task.FromResult(new VerifyTokenResultDTO(IsValid: true, userId, Message: "Token is valid"));
 	}
 }
