4b36c497ae3e42273559bf9220ed9767640ae07c
aalimuhammed <abdelrahman.ali@siac-construction.com>
2026-09-10 10:01:18 +0300

    FIX EXCEL ISSUE

--- Application/CompanyPosts.Application/Abstraction/IExcelExportService.cs
+++ Application/CompanyPosts.Application/Abstraction/IExcelExportService.cs
@@ -1,13 +1,7 @@
 ﻿namespace CompanyPost.Application.Abstraction
 {
-<<<<<<< Updated upstream
-    public interface IExcelExportService
-=======
     public interface IExcelExportService<T> where T : class
->>>>>>> Stashed changes
     {
-        byte[] ExportToExcel<T>(
-        IEnumerable<T> data,
-        IEnumerable<ExcelColumn<T>> columns);
+        byte[] ExportToExcel<T>(IEnumerable<T> data);
     }
 }
--- Infrastructure/CompanyPost.Infrastructure/Services/ExcelExportService.cs
+++ Infrastructure/CompanyPost.Infrastructure/Services/ExcelExportService.cs
@@ -1,65 +1,19 @@
 ﻿namespace CompanyPost.Infrastructure.Services;
 
-public class ExcelExportService : IExcelExportService
+public class ExcelExportService<T> : IExcelExportService<T> where T : class
 {
-<<<<<<< Updated upstream
-    public byte[] ExportToExcel<T>(
-        IEnumerable<T> data,
-        IEnumerable<ExcelColumn<T>> columns)
-=======
-    internal sealed class ExcelExportService<T> 
-        : IExcelExportService<T> where T : class
->>>>>>> Stashed changes
+    public byte[] ExportToExcel(IEnumerable<T> data)
     {
         using var workbook = new XLWorkbook();
+        var worksheet = workbook.Worksheets.Add(typeof(T).Name);
 
-        var worksheet = workbook.Worksheets.Add("Report");
-
-        var columnList = columns.ToList();
-
-        // Headers
-        for (int columnIndex = 0; columnIndex < columnList.Count; columnIndex++)
-        {
-            worksheet.Cell(1, columnIndex + 1).Value =
-                columnList[columnIndex].ColumnName;
-        }
-
-        // Data
-        var rowIndex = 2;
-
-        foreach (var item in data)
-        {
-            for (int columnIndex = 0; columnIndex < columnList.Count; columnIndex++)
-            {
-                var value = columnList[columnIndex].ColumnValue(item);
-
-                worksheet.Cell(rowIndex, columnIndex + 1).Value =
-                    value?.ToString() ?? string.Empty;
-            }
-
-            rowIndex++;
-        }
+        worksheet.Cell(1, 1).InsertTable(data);
 
         worksheet.Columns().AdjustToContents();
 
         using var stream = new MemoryStream();
-
         workbook.SaveAs(stream);
 
         return stream.ToArray();
     }
 }
-//public byte[] ExportToExcel(IEnumerable<T> data)
-//{
-//    using var workbook = new XLWorkbook();
-//    var worksheet = workbook.Worksheets.Add(typeof(T).Name);
-
-//    worksheet.Cell(1, 1).InsertTable(data);
-
-//    worksheet.Columns().AdjustToContents();
-
-//    using var stream = new MemoryStream();
-//    workbook.SaveAs(stream);
-
-//    return stream.ToArray();
-//}
